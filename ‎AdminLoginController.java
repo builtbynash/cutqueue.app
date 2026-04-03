88f6cdb2cad5397096473175418fdfe23c09aa88
Nash Basti M. Estrada <bastinashestrada@gmail.com>
2026-04-03 09:48:11 +0800

    AdminLoginController.java

---
+++ ‎AdminLoginController.java
@@ -0,0 +1,30 @@
+‎package controller;
+‎
+‎import javafx.fxml.FXML;
+‎import javafx.fxml.FXMLLoader;
+‎import javafx.scene.Scene;
+‎import javafx.stage.Stage;
+‎import javafx.scene.control.*;
+‎
+‎public class AdminLoginController {
+‎
+‎    @FXML private TextField usernameField;
+‎    @FXML private PasswordField passwordField;
+‎
+‎    @FXML
+‎    private void handleLogin() throws Exception {
+‎
+‎        if (usernameField.getText().equals("admin") &&
+‎            passwordField.getText().equals("1234")) {
+‎
+‎            Stage stage = (Stage) usernameField.getScene().getWindow();
+‎
+‎            stage.setScene(new Scene(
+‎                FXMLLoader.load(getClass().getResource("/view/dashboard.fxml"))
+‎            ));
+‎
+‎        } else {
+‎            new Alert(Alert.AlertType.ERROR, "Invalid admin login").show();
+‎        }
+‎    }
+‎}
