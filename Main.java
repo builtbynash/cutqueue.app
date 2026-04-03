b6985412891157f6165e24e76d4dc2f059a4c4c9
Nash Basti M. Estrada <bastinashestrada@gmail.com>
2026-04-03 09:41:27 +0800

    Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Main extends Application {
+@Override
+public void start(Stage stage) throws Exception {
+stage.setScene(new Scene(
+FXMLLoader.load(getClass().getResource("/view/start.fxml"))
+));
+stage.setTitle("CutQueue App");
+stage.show();
+}
+public static void main(String[] args) {
+launch();
+}
+}
