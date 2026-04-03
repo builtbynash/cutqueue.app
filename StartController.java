ebf1266d7e0c9c1441986403e3bdb9f074ab6298
Nash Basti M. Estrada <bastinashestrada@gmail.com>
2026-04-03 09:47:01 +0800

    StartController.java

---
+++ StartController.java
@@ -0,0 +1,29 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+‎public class StartController {
+‎
+‎    @FXML
+‎    private void handleStart() throws Exception {
+‎        Stage stage = (Stage) Stage.getWindows().get(0);
+‎
+‎        stage.setScene(new Scene(
+‎            FXMLLoader.load(getClass().getResource("/view/booking.fxml"))
+‎        ));
+‎    }
+‎
+‎    @FXML
+‎    private void handleAdmin() throws Exception {
+‎        Stage stage = (Stage) Stage.getWindows().get(0);
+‎
+‎        stage.setScene(new Scene(
+‎            FXMLLoader.load(getClass().getResource("/view/admin_login.fxml"))
+‎        ));
+‎    }
+‎}
+
+  
+  
